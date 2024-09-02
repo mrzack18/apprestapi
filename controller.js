5da35c1a1c83619383e7b59ccf8a9d6d8f531bff
unknown <zkymhmmd18@gmail.com>
2024-09-02 11:19:56 +0700

    berhasil menampilkan semua data mahasiswa

--- controller.js
+++ controller.js
@@ -1,8 +1,19 @@
-'use strict';
+"use strict";
 
-var response = require('./rest');
-var connection = require('./koneksi');
+var response = require("./rest");
+var connection = require("./koneksi");
 
-exports.index = function(req, res){
-    response.ok("Aplikasi REST API ku berjalan",res)
-}
+exports.index = function (req, res) {
+  response.ok("Aplikasi REST API ku berjalan", res);
+};
+
+// menampilkan semua data mahasiswa
+exports.tampilsemuamahasiswa = function (req, res) {
+  connection.query("SELECT * FROM mahasiswa", function (error, rows, fields) {
+    if (error) {
+      connection.log(error);
+    } else {
+      response.ok(rows, res);
+    }
+  });
+};
